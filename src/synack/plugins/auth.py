3c999b564611e33b57e193b2b6d85fd60cbe967d
Dylan Wilson <bamhm182@gmail.com>
2025-01-04 23:55:11 +0000

    duo: slight refactor, still terrible

--- src/synack/plugins/auth.py
+++ src/synack/plugins/auth.py
@@ -65,8 +65,7 @@ class Auth(Plugin):
                       res.text)
         return m.group(1)
 
-    def get_duo_push(self, duo_auth_url):
-        """Make Duo send a push notification"""
+    def get_duo_push_variables(self, duo_auth_url):
         headers = {
             'Sec-Ch-Ua': '"Chromium";v="131", "Not_A Brand";v="24"',
             'Sec-Ch-Ua-Mobile': '?0',
@@ -78,191 +77,191 @@ class Auth(Plugin):
             'Sec-Fetch-Dest': 'document'
         }
         res = self.api.request('GET', duo_auth_url, include_std_headers=False)
-        headers = {
-            'Sec-Ch-Ua': '"Chromium";v="131", "Not_A Brand";v="24"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"Linux"',
-            'Referer': res.url,
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Dest': 'document',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-        data = {
-            'tx': re.search('<input type="hidden" name="tx" value="([^"]*)"', res.text).group(1),
-            'parent': re.search('<input type="hidden" name="parent" value="([^"]*)"', res.text).group(1),
-            '_xsrf': re.search('<input type="hidden" name="_xsrf" value="([^"]*)"', res.text).group(1),
-            'version': re.search('<input type="hidden" name="version" value="([^"]*)"', res.text).group(1),
-            'akey': re.search('<input type="hidden" name="akey" value="([^"]*)"', res.text).group(1),
-            'has_session_trust_analysis_feature': re.search('<input type="hidden" name="has_session_trust_analysis_feature" value="([^"]*)"', res.text).group(1),
-            'session_trust_extension_id': re.search('<input type="hidden" name="session_trust_extension_id" value="([^"]*)"', res.text).group(1),
-            'java_version': re.search('<input type="hidden" name="java_version" value="([^"]*)"', res.text).group(1),
-            'flash_version': re.search('<input type="hidden" name="flash_version" value="([^"]*)"', res.text).group(1),
-            'screen_resolution_width': '3422',
-            'screen_resolution_height': '1465',
-            'extension_instance_key': '',
-            'color_depth': '24',
-            'has_touch_capability': 'false',
-            'ch_ua_error': '',
-            'client_hints': 'eyJicmFuZHMiOlt7ImJyYW5kIjoiQ2hyb21pdW0iLCJ2ZXJzaW9uIjoiMTMxIn0seyJicmFuZCI6Ik5vdF9BIEJyYW5kIiwidmVyc2lvbiI6IjI0In1dLCJmdWxsVmVyc2lvbkxpc3QiOltdLCJtb2JpbGUiOmZhbHNlLCJwbGF0Zm9ybSI6IkxpbnV4IiwicGxhdGZvcm1WZXJzaW9uIjoiIiwidWFGdWxsVmVyc2lvbiI6IiJ9',
-            'is_cef_browser': 'false',
-            'is_ipad_os': 'false',
-            'is_ie_compatibility_mode': '',
-            'is_user_verifying_platform_authenticator_available': 'false',
-            'user_verifying_platform_authenticator_available_error': '',
-            'acting_ie_version': '',
-            'react_support': 'false',
-            'react_support_error_message': ''
-        }
-        sid = re.search('sid=([^&]*)', res.url).group(1)
-        base_url = re.search('(https.*duosecurity\.com)/', res.url).group(1)
-        res2 = self.api.request('POST', res.url, include_std_headers=False, headers=headers, data=data)
-        headers = {
-            'Sec-Ch-Ua': '"Chromium";v="131", "Not_A Brand";v="24"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"Linux"',
-            'Referer': f'{base_url}/frame/v4/preauth/healthcheck?sid={sid}',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Dest': 'document',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        }
-        res_return = self.api.request('GET', f'{base_url}/frame/v4/return', include_std_headers=False, headers=headers, query={"sid": sid})
-        xsrf = re.search('<input type="hidden" name="_xsrf" value="([^"]*)"', res_return.text).group(1)
-        headers = {
-            'Sec-Ch-Ua': '"Chromium";v="131", "Not_A Brand";v="24"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"Linux"',
-            'Referer': res_return.url,
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Dest': 'document',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-        data = {
-            'tx': re.search('<input type="hidden" name="tx" value="([^"]*)"', res_return.text).group(1),
-            'parent': re.search('<input type="hidden" name="parent" value="([^"]*)"', res_return.text).group(1),
-            '_xsrf': re.search('<input type="hidden" name="_xsrf" value="([^"]*)"', res_return.text).group(1),
-            'version': re.search('<input type="hidden" name="version" value="([^"]*)"', res_return.text).group(1),
-            'akey': re.search('<input type="hidden" name="akey" value="([^"]*)"', res_return.text).group(1),
-            'has_session_trust_analysis_feature': re.search('<input type="hidden" name="has_session_trust_analysis_feature" value="([^"]*)"', res_return.text).group(1),
-            'session_trust_extension_id': re.search('<input type="hidden" name="session_trust_extension_id" value="([^"]*)"', res_return.text).group(1),
-            'java_version': re.search('<input type="hidden" name="java_version" value="([^"]*)"', res_return.text).group(1),
-            'flash_version': re.search('<input type="hidden" name="flash_version" value="([^"]*)"', res_return.text).group(1),
-            'screen_resolution_width': '3422',
-            'screen_resolution_height': '1465',
-            'extension_instance_key': '',
-            'color_depth': '24',
-            'has_touch_capability': 'false',
-            'ch_ua_error': '',
-            'client_hints': 'eyJicmFuZHMiOlt7ImJyYW5kIjoiQ2hyb21pdW0iLCJ2ZXJzaW9uIjoiMTMxIn0seyJicmFuZCI6Ik5vdF9BIEJyYW5kIiwidmVyc2lvbiI6IjI0In1dLCJmdWxsVmVyc2lvbkxpc3QiOltdLCJtb2JpbGUiOmZhbHNlLCJwbGF0Zm9ybSI6IkxpbnV4IiwicGxhdGZvcm1WZXJzaW9uIjoiIiwidWFGdWxsVmVyc2lvbiI6IiJ9',
-            'is_cef_browser': 'false',
-            'is_ipad_os': 'false',
-            'is_ie_compatibility_mode': '',
-            'is_user_verifying_platform_authenticator_available': 'false',
-            'user_verifying_platform_authenticator_available_error': '',
-            'acting_ie_version': '',
-            'react_support': 'false',
-            'react_support_error_message': ''
-        }
-        res_auth_again_before_prompt = self.api.request('POST', res.url, include_std_headers=False, headers=headers, data=data)
+        if res.status_code == 200:
+            return {
+                'post_data': {
+                    'tx': re.search('<input type="hidden" name="tx" value="([^"]*)"', res.text).group(1),
+                    'parent': re.search('<input type="hidden" name="parent" value="([^"]*)"', res.text).group(1),
+                    '_xsrf': re.search('<input type="hidden" name="_xsrf" value="([^"]*)"', res.text).group(1),
+                    'version': re.search('<input type="hidden" name="version" value="([^"]*)"', res.text).group(1),
+                    'akey': re.search('<input type="hidden" name="akey" value="([^"]*)"', res.text).group(1),
+                    'has_session_trust_analysis_feature': re.search('<input type="hidden" name="has_session_trust_analysis_feature" value="([^"]*)"', res.text).group(1),
+                    'session_trust_extension_id': re.search('<input type="hidden" name="session_trust_extension_id" value="([^"]*)"', res.text).group(1),
+                    'java_version': re.search('<input type="hidden" name="java_version" value="([^"]*)"', res.text).group(1),
+                    'flash_version': re.search('<input type="hidden" name="flash_version" value="([^"]*)"', res.text).group(1),
+                    'screen_resolution_width': '3422',
+                    'screen_resolution_height': '1465',
+                    'extension_instance_key': '',
+                    'color_depth': '24',
+                    'has_touch_capability': 'false',
+                    'ch_ua_error': '',
+                    'client_hints': 'eyJicmFuZHMiOlt7ImJyYW5kIjoiQ2hyb21pdW0iLCJ2ZXJzaW9uIjoiMTMxIn0seyJicmFuZCI6Ik5vdF9BIEJyYW5kIiwidmVyc2lvbiI6IjI0In1dLCJmdWxsVmVyc2lvbkxpc3QiOltdLCJtb2JpbGUiOmZhbHNlLCJwbGF0Zm9ybSI6IkxpbnV4IiwicGxhdGZvcm1WZXJzaW9uIjoiIiwidWFGdWxsVmVyc2lvbiI6IiJ9',
+                    'is_cef_browser': 'false',
+                    'is_ipad_os': 'false',
+                    'is_ie_compatibility_mode': '',
+                    'is_user_verifying_platform_authenticator_available': 'false',
+                    'user_verifying_platform_authenticator_available_error': '',
+                    'acting_ie_version': '',
+                    'react_support': 'false',
+                    'react_support_error_message': ''
+                },
+                'sid': re.search('sid=([^&]*)', res.url).group(1),
+                'url_last': res.url,
+                'url_base': re.search('(https.*duosecurity\.com)/', res.url).group(1)
+            }
 
-        # Doing a get on /fram/v4/auth/prompt/data
+    def get_duo_push_vars_post(self, push_vars):
         headers = {
             'Sec-Ch-Ua': '"Chromium";v="131", "Not_A Brand";v="24"',
             'Sec-Ch-Ua-Mobile': '?0',
             'Sec-Ch-Ua-Platform': '"Linux"',
-            'Referer': f'{base_url}/frame/v4/auth/prompt?sid={sid}',
+            'Referer': push_vars.get('url_last', ''),
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Dest': 'document',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        res = self.api.request('POST', push_vars.get('url_last', ''), include_std_headers=False, headers=headers, data=push_vars.get('post_data', {}))
+        if res.status_code == 200:
+            push_vars['url_last'] = res.url
+        return push_vars
+
+    def get_duo_push_xsrf_token(self, push_vars):
+        headers = {
+            'Sec-Ch-Ua': '"Chromium";v="131", "Not_A Brand";v="24"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': '"Linux"',
+            'Referer': f'{push_vars.get("url_base", "")}/frame/v4/preauth/healthcheck?sid={push_vars.get("sid", "")}',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Dest': 'document',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        }
+        res = self.api.request('GET', f'{push_vars.get("url_base", "")}/frame/v4/return', include_std_headers=False, headers=headers, query={"sid": push_vars.get('sid', '')})
+        if res.status_code == 200:
+            push_vars['xsrf'] = re.search('<input type="hidden" name="_xsrf" value="([^"]*)"', res.text).group(1)
+        return push_vars
+
+    def get_duo_push_method_data(self, push_vars):
+        headers = {
+            'Sec-Ch-Ua': '"Chromium";v="131", "Not_A Brand";v="24"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': '"Linux"',
+            'Referer': f'{push_vars.get("url_base", "")}/frame/v4/auth/prompt?sid={push_vars.get("sid", "")}',
             'Sec-Fetch-Site': 'same-origin',
             'Sec-Fetch-Mode': 'cors',
             'Sec-Fetch-Dest': 'empty',
             'Accept': '*/*',
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-            'X-Xsrftoken': xsrf,
+            'X-Xsrftoken': push_vars.get('xsrf', ''),
         }
         query = {
             'post_auth_action': 'OIDC_EXIT',
             'browser_features': '{"touch_supported":false,"platform_authenticator_status":"unavailable","webauthn_supported":true}',
-            'sid': sid
+            'sid': push_vars.get('sid', '')
         }
-        res_get_prompt_options = self.api.request('GET', f'{base_url}/frame/v4/auth/prompt/data', include_std_headers=False, headers=headers, query=query)
-        for method in res_get_prompt_options.json().get('response', {}).get('auth_method_order', []):
-            print(method)
-            if method.get('factor', '') == 'Duo Push':
-                prompt_device_key = method.get('deviceKey', '')
-                print(f'Device Key: {prompt_device_key}')
-                break
+        res = self.api.request('GET', f'{push_vars.get("url_base", "")}/frame/v4/auth/prompt/data', include_std_headers=False, headers=headers, query=query)
+        if res.status_code == 200:
+            for method in res.json().get('response', {}).get('auth_method_order', []):
+                if method.get('factor', '') == 'Duo Push':
+                    push_vars['prompt_device_key'] = method.get('deviceKey', '')
+                    break
 
-        for phone in res_get_prompt_options.json().get('response', {}).get('phones', []):
-            print(phone)
-            if phone.get('key', '') == prompt_device_key:
-                prompt_device_index = phone.get('index', '')
-                print(f'Device Index: {prompt_device_index}')
+            for phone in res.json().get('response', {}).get('phones', []):
+                if phone.get('key', '') == push_vars.get('prompt_device_key', ''):
+                    push_vars['prompt_device_index'] = phone.get('index', '')
+        return push_vars
 
-        # Doing the POST that should actuall send the push notification
+    def get_duo_push_notification_txid(self, push_vars):
+        # Doing the POST that should actually send the push notification
         headers = {
             'Sec-Ch-Ua': '"Chromium";v="131", "Not_A Brand";v="24"',
             'Sec-Ch-Ua-Mobile': '?0',
             'Sec-Ch-Ua-Platform': '"Linux"',
-            'Referer': f'{base_url}/frame/v4/auth/prompt?sid={sid}',
+            'Referer': f'{push_vars.get("url_base", "")}/frame/v4/auth/prompt?sid={push_vars.get("sid", "")}',
             'Sec-Fetch-Site': 'same-origin',
             'Sec-Fetch-Mode': 'cors',
             'Sec-Fetch-Dest': 'empty',
             'Accept': '*/*',
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-            'X-Xsrftoken': xsrf,
+            'X-Xsrftoken': push_vars.get('xsrf', ''),
         }
         data = {
-            'device': prompt_device_index,
+            'device': push_vars.get('prompt_device_index', ''),
             'factor': 'Duo Push',
             'postAuthDestination': 'OIDC_EXIT',
             'browser_features': '{"touch_supported":false,"platform_authenticator_status":"unavailable","webauthn_supported":true}',
-            'sid': sid
+            'sid': push_vars.get('sid', '')
         }
-        res3 = self.api.request('POST', f'{base_url}/frame/v4/prompt', include_std_headers=False, headers=headers, data=data)
-        txid = res3.json().get('response', {}).get('txid', '')
+        res = self.api.request('POST', f'{push_vars.get("url_base", "")}/frame/v4/prompt', include_std_headers=False, headers=headers, data=data)
+        if res.status_code == 200:
+            push_vars['txid'] = res.json().get('response', {}).get('txid', '')
+        return push_vars
 
-        # Doing POST requests to check status of push notification
+    def get_duo_push_status(self, push_vars):
         headers = {
             'Sec-Ch-Ua': '"Chromium";v="131", "Not_A Brand";v="24"',
             'Sec-Ch-Ua-Mobile': '?0',
             'Sec-Ch-Ua-Platform': '"Linux"',
-            'Referer': f'{base_url}/frame/v4/auth/prompt?sid={sid}',
+            'Referer': f'{push_vars.get("url_base", "")}/frame/v4/auth/prompt?sid={push_vars.get("sid", "")}',
             'Sec-Fetch-Site': 'same-origin',
             'Sec-Fetch-Mode': 'cors',
             'Sec-Fetch-Dest': 'empty',
             'Accept': '*/*',
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-            'X-Xsrftoken': xsrf,
+            'X-Xsrftoken': push_vars.get('xsrf', ''),
         }
         data = {
-            'txid': txid,
-            'sid': sid
+            'txid': push_vars.get('txid', ''),
+            'sid': push_vars.get('sid', '')
         }
 
         for i in range(5):
-            get_status = self.api.request('POST', f'{base_url}/frame/v4/status', include_std_headers=False, headers=headers, data=data)
-            status = get_status.json().get('response', {}).get('result', '')
+            res = self.api.request('POST', f'{push_vars.get("url_base", "")}/frame/v4/status', include_std_headers=False, headers=headers, data=data)
+            status = res.json().get('response', {}).get('result', '')
             if status == 'SUCCESS':
                 break
             time.sleep(5)
 
+    def get_duo_push_grant_token(self, push_vars):
+        headers = {
+            'Sec-Ch-Ua': '"Chromium";v="131", "Not_A Brand";v="24"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': '"Linux"',
+            'Referer': f'{push_vars.get("url_base", "")}/frame/v4/auth/prompt?sid={push_vars.get("sid", "")}',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Dest': 'empty',
+            'Accept': '*/*',
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'X-Xsrftoken': push_vars.get('xsrf', ''),
+        }
         data = {
-            'sid': sid,
-            'txid': txid,
+            'sid': push_vars.get('sid', ''),
+            'txid': push_vars.get('txid', ''),
             'factor': 'Duo Push',
-            'device_key': prompt_device_key,
-            '_xsrf': xsrf,
+            'device_key': push_vars.get('prompt_device_key', ''),
+            '_xsrf': push_vars.get('xsrf', ''),
             'dampen_choice': 'false'
         }
-        oidc_exit = self.api.request('POST', f'{base_url}/frame/v4/oidc/exit', include_std_headers=False, headers=headers, data=data)
-        print(oidc_exit.url)
-        grant_token = re.search('grant_token=([^&]*)', oidc_exit.url).group(1)
-        return grant_token
+        res = self.api.request('POST', f'{push_vars.get("url_base", "")}/frame/v4/oidc/exit', include_std_headers=False, headers=headers, data=data)
+        if res.status_code == 200:
+            push_vars['grant_token'] = re.search('grant_token=([^&]*)', res.url).group(1)
 
+        return push_vars
 
+    def get_duo_push(self, duo_auth_url):
+        """Make Duo send a push notification"""
+        push_vars = self.get_duo_push_variables(duo_auth_url)
+        self.get_duo_push_vars_post(push_vars)
+        push_vars = self.get_duo_push_xsrf_token(push_vars)
+        self.get_duo_push_vars_post(push_vars)
+        push_vars = self.get_duo_push_method_data(push_vars)
+        push_vars = self.get_duo_push_notification_txid(push_vars)
+        self.get_duo_push_status(push_vars)
+        push_vars = self.get_duo_push_grant_token(push_vars)
+        return push_vars.get('grant_token', '')
 
     def get_login_grant_token(self, csrf, progress_token):
         """Get grant token from authy totp verification"""
