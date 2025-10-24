Derb237/SynackAPI

commit c406ca9d88b184982b1d08173384eb7fa34432e1

improved request and re-login handling

src/synack/plugins/api.py
@@ -144,23 +144,27 @@ class Api(Plugin):
                         f"\n\tData: {data}" +
                         f"\n\tContent: {res.content}")
 
-        if res.status_code in [ 400, 401, 403 ]:
-            print('Request failed... Bailing!')
-            print(f'\t({res.status_code} - {res.reason}) {res.url}')
+        reason_failed = 'Request failed'
+        if res.status_code in [ 400, 401 ]:
+            reason_failed = 'Request failed'
+        elif res.status_code == 403:
+            reason_failed = 'Logged out'
+        elif res.status_code == 412:
+            fail_reason = 'Mission already claimed'
         elif res.status_code == 429:
-            print('Too many requests! Slow down there, cowpoke!')
-            print(f'\t({res.status_code} - {res.reason}) {res.url}')
+            self._debug.log('Too many requests', f'({res.status_code} - {res.reason}) {res.url}')
             if attempts < 5:
-                attempts += 1
-                print('\tRetrying in 30 seconds...')
+                self._debug.log('Pausing', 'Retrying in 30 seconds...')
                 time.sleep(30)
+                attempts += 1
                 return self.request(method, path, attempts, **kwargs)
         elif res.status_code >= 400:
-            print(f'Request failed...')
-            print(f'\t({res.status_code} - {res.reason}) {res.url}')
+            self._debug.log(f'Request failed', f'({res.status_code} - {res.reason}) {res.url}')
             if attempts < 5:
-                print(f'\tRetry attempt #{attempts + 1}')
+                self._debug.log('Retrying', f'Attempt #{attempts + 1}')
                 attempts += 1
                 return self.request(method, path, attempts, **kwargs)
+
+        self._debug.log('Mission already claimed', f'({res.status_code} - {res.reason}) {res.url}')
 
         return res

src/synack/plugins/auth.py
@@ -18,7 +18,7 @@ class Auth(Plugin):
 
     def get_api_token(self):
         """Log in to get a new API token."""
-        if self._users.get_profile():
+        if self._api.request('HEAD', 'profiles/me').status_code == 200:
             return self._state.api_token
         csrf = self.get_login_csrf()
         duo_auth_url = None

src/synack/plugins/missions.py
@@ -117,6 +117,8 @@ class Missions(Plugin):
                                per_page=per_page)
                 ret.extend(new)
             return ret
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
         return []
 
     def get_approved(self, **kwargs):
@@ -152,6 +154,8 @@ class Missions(Plugin):
                                 query=query)
         if res.status_code == 204:
             return int(res.headers.get('x-count', 0))
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
         return 0
 
     def get_evidences(self, mission):
@@ -172,6 +176,8 @@ class Missions(Plugin):
             ret["structuredResponse"] = mission["validResponses"][1]["value"]
 
             return ret
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
 
     def get_in_review(self, **kwargs):
         """Get a list of missions currently in review"""
@@ -184,6 +190,8 @@ class Missions(Plugin):
                                 'tasks/v2/researcher/claimed_amount')
         if res.status_code == 200:
             return int(res.json().get('claimedAmount', '0'))
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
 
     def get_wallet_limit(self):
         """Get Current Mission Wallet Limit"""
@@ -191,6 +199,8 @@ class Missions(Plugin):
                                 'profiles/me')
         if res.status_code == 200:
             return int(res.json().get('claim_limit', '0'))
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
 
     def set_claimed(self, mission):
         """Try to claim a single mission
@@ -236,6 +246,8 @@ class Missions(Plugin):
                     ret["title"] = mission["title"]
                     ret["codename"] = mission["listingCodename"]
                     return ret
+                elif res.status_code == 403 and self._state.login:
+                    self._auth.get_api_token()
 
     def set_status(self, mission, status):
         """Interact with single mission

src/synack/plugins/scratchspace.py
@@ -48,6 +48,8 @@ class Scratchspace(Plugin):
                         with open(dest_file, 'wb') as fp:
                             fp.write(res.content)
                             downloads.append(dest_file)
+                    elif res.status_code == 403 and self._state.login:
+                        self._auth.get_api_token()
         return downloads
 
     def set_file(self, content, filename, target=None, codename=None):

src/synack/plugins/targets.py
@@ -100,6 +100,8 @@ class Targets(Plugin):
         if res.status_code == 200:
             self._db.add_categories(res.json())
             return self._db.categories
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
 
     def get_assets(self, target=None, asset_type=None, host_type=None, active='true',
                    scope=['in', 'discovered'], sort='location', sort_dir='asc',
@@ -150,6 +152,8 @@ class Targets(Plugin):
                 if self._state.use_scratchspace:
                     self._scratchspace.set_assets_file(res.text, target=target)
                 return res.json()
+            elif res.status_code == 403 and self._state.login:
+                self._auth.get_api_token()
 
     def get_attachments(self, target=None, **kwargs):
         """Get the attachments of a target."""
@@ -162,6 +166,8 @@ class Targets(Plugin):
         res = self._api.request('GET', f'targets/{target.slug}/resources')
         if res.status_code == 200:
             return res.json()
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
 
     def get_connected(self):
         """Return information about the currenly selected target"""
@@ -181,6 +187,8 @@ class Targets(Plugin):
                 "status": status
             }
             return ret
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
 
     def get_connections(self, target=None, **kwargs):
         """Get the connection details of a target."""
@@ -193,6 +201,8 @@ class Targets(Plugin):
         res = self._api.request('GET', "listing_analytics/connections", query={"listing_id": target.slug})
         if res.status_code == 200:
             return res.json()["value"]
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
 
     def get_credentials(self, **kwargs):
         """Get Credentials for a target"""
@@ -205,6 +215,8 @@ class Targets(Plugin):
                                     '/credentials')
             if res.status_code == 200:
                 return res.json()
+            elif res.status_code == 403 and self._state.login:
+                self._auth.get_api_token()
 
     def get(self, status='registered', query_changes={}):
         """Get information about targets returned from a query"""
@@ -225,6 +237,8 @@ class Targets(Plugin):
         if res.status_code == 200:
             self._db.add_targets(res.json(), is_registered=True)
             return res.json()
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
 
     def get_registered_summary(self):
         """Get information on your registered targets"""
@@ -235,6 +249,8 @@ class Targets(Plugin):
             ret = dict()
             for t in res.json():
                 ret[t['id']] = t
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
         return ret
 
     def get_scope(self, **kwargs):
@@ -338,6 +354,8 @@ class Targets(Plugin):
         res = self._api.request('GET', "listing_analytics/categories", query=query)
         if res.status_code == 200:
             return res.json()["value"]
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
 
     def get_submissions_summary(self, target=None, hours_ago=None, **kwargs):
         """Get a summary of the submission analytics of a target."""
@@ -353,6 +371,8 @@ class Targets(Plugin):
         res = self._api.request('GET', "listing_analytics/submissions", query=query)
         if res.status_code == 200:
             return res.json()["value"]
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()
 
     def get_unregistered(self):
         """Get slugs of all unregistered targets"""
@@ -382,6 +402,8 @@ class Targets(Plugin):
             res = self._api.request('PUT', 'launchpoint', data={'listing_id': slug})
             if res.status_code == 200:
                 return self.get_connected()
+            elif res.status_code == 403 and self._state.login:
+                self._auth.get_api_token()
 
     def set_registered(self, targets=None):
         """Register all unregistered targets"""
@@ -395,6 +417,8 @@ class Targets(Plugin):
                                     data=data)
             if res.status_code == 200:
                 ret.append(t)
+            elif res.status_code == 403 and self._state.login:
+                self._auth.get_api_token()
         if len(targets) >= 15:
             ret.extend(self.set_registered())
         return ret

src/synack/plugins/transactions.py
@@ -21,3 +21,5 @@ class Transactions(Plugin):
         res = self._api.request('HEAD', 'transactions')
         if res.status_code == 200:
             return json.loads(res.headers.get('x-balance'))
+        elif res.status_code == 403 and self._state.login:
+            self._auth.get_api_token()

commit 53a5be77438b3483a65d027aa0d2afbe894dfde4

Fix spurious debug logging in api.py
Fixed two bugs in the request error handling:

1. Line 153: Fixed typo where 'fail_reason' was set instead of 'reason_failed'
   for HTTP 412 status codes

2. Line 168: Moved unconditional debug log into a conditional that only
   logs terminal failures (400, 401, 403, 412)

Previously, when debug mode was enabled, every HTTP request would log
'MISSION ALREADY CLAIMED' regardless of the actual status code or context.
This was caused by an unconditional debug.log() call that always executed.

Now, 'Mission already claimed' only appears for actual HTTP 412 errors.

Bug introduced in commit c406ca9d (March 2, 2025)

src/synack/plugins/api.py
@@ -150,7 +150,7 @@ class Api(Plugin):
         elif res.status_code == 403:
             reason_failed = 'Logged out'
         elif res.status_code == 412:
-            fail_reason = 'Mission already claimed'
+            reason_failed = 'Mission already claimed'
         elif res.status_code == 429:
             self._debug.log('Too many requests', f'({res.status_code} - {res.reason}) {res.url}')
             if attempts < 5:
@@ -165,6 +165,8 @@ class Api(Plugin):
                 attempts += 1
                 return self.request(method, path, attempts, **kwargs)
 
-        self._debug.log('Mission already claimed', f'({res.status_code} - {res.reason}) {res.url}')
+        # Log terminal failures (non-retryable errors)
+        if res.status_code in [400, 401, 403, 412]:
+            self._debug.log(reason_failed, f'({res.status_code} - {res.reason}) {res.url}')
 
         return res